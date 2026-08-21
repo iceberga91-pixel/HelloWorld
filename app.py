483aef6b243c06551a1b152e142cb0149b3533e4
Joao Berga <iceberga91@gmail.com>
2026-08-21 16:30:11 +0100

    installed, pip3, flask, flask-alchemy created files: static, env, templates added base.html, helloworld.html, index.html, app, gato.jpeg

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/helloworld')
+def helloworld():
+    return render_template('helloworld.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
